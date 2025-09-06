028e27516cde223d6fc3c82c35889bface25e2f0
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-07 01:33:37 +0330

    Init, Added, Defined: the "glyph" data type was defined. Later, `lytWord` would just be a linked-list of `lytGlyph`. The reason `lytGlyphVector` exists is fast O(1) access

---
+++ src/glyph.h
@@ -0,0 +1,50 @@
+#ifndef GLYPH_H
+#define GLYPH_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <uchar.h>
+
+#include "font.h"
+#include "tyydecl.h"
+
+struct lytGlyph
+{
+  char32_t codepoint;
+  const char *name;
+  int font_id;
+  double point_size;
+  double slant;
+  double height;
+
+  double width;
+  double ascent;
+  double descent;
+  double bbox_xmin;
+  double bbox_ymin;
+  double bbox_xmax;
+  double bbox_ymax;
+
+  double pos_x;
+  double pos_y;
+
+  bool underline;
+  bool overstrike;
+  bool faux_bold;
+  bool visible;
+
+  lytFont *parent_font;
+  lytGlyph *next;
+};
+
+struct lytGlyphVector
+{
+  lytGlyph *data;
+  size_t capacity;
+  size_t count;
+};
+
+#endif
