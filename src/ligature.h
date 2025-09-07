c71fc8a84b8fbdf2dfad2bdd60d5e674d254f1ca
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-07 11:23:43 +0330

    Init, Added, Defined: the ligature type was defined and flags were also defined. The list type for ligatures was also defined, as per change to `device.h` (before this change, I had decided to use an array, but I decided to use a linked list instead)

---
+++ src/ligature.h
@@ -0,0 +1,43 @@
+#ifndef LIGATURE_H
+#define LIGATURE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "font.h"
+#include "tyydecl.h"
+
+#define LYT_LIGF_DISCRETIONARY (1U << 0)
+#define LYT_LIGF_END_OF_WORD (1U << 1)
+#define LYT_LIGF_REQUIRES_KERN (1U << 2)
+#define LYT_LIGF_LEGACY_ONLY (1U << 3)
+#define LYT_LIGF_MANDATORY (1U << 4)
+#define LYT_LIGF_SYNTHETIC (1U << 5)
+
+struct lytLigature
+{
+  char sequence[8];
+  char32_t codepoint;
+
+  const char *glyph_name;
+  int glyph_id;
+
+  double advance_x;
+  double advance_y;
+
+  lytFont *parent_font;
+  unsigned flags;
+
+  lytLigature *next;
+};
+
+struct lytLigatureList
+{
+  lytLigature *head;
+  lytLigature *tail;
+};
+
+#endif
